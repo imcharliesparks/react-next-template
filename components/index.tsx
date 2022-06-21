e8305a6195e855e02a98c1990e66f9266640ec0d
Charlie Sparks <sparksfullstack@gmail.com>
2022-06-21 14:33:13 -0700

    Charlie | Removed redux usage

---
+++ components/index.tsx
@@ -0,0 +1,3 @@
+import * as React from 'react'
+
+export const index = () => <div>howdy</div>
--- redux/store.ts
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { initialReducer } from './reducers/initialReducer'
-
-// TODO: Fix type error
-export default configureStore({
-	reducer: {
-		initialReducer
-	}
-})
